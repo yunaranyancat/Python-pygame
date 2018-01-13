e04dcb6db7346f2f363c82c27f66d310a5a5b38e
M. Zulfiqar WARDI <mzulfiqar.wardi@gmail.com>
2018-01-13 15:34:27 +0100

    Drawing and filling shapes into the pygame display screen. Creating a rectangle is possible using pygame.draw and pygame.display.fill, but, the latter has higher graphic processing speed than  the draw method.

--- tutorials/idle-tutorial6.py
+++ tutorials/idle-tutorial6.py
@@ -5,7 +5,8 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 
-gameDisplay = pygame.display.set_mode((800,600)) pygame.display.set_caption('Slither')
+gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption('Slither')
 
 
 gameExit = False
@@ -18,7 +19,12 @@ while not gameExit:
     gameDisplay.fill(white)
     #space to render graphics
     #i.e : https://www.pygame.org/docs/ref/draw.html
-    
+    #x coord is going to right side -> +, y coord is going to down side -> +
+    pygame.draw.rect(gameDisplay, black,[400,300,10,100]) #surface,color,x-position,y-posiiton,width,height
+    #pygame.draw.rect(gameDisplay, red,[400,300,10,10]) 
+    gameDisplay.fill(red, rect=[200,200,50,50])
+
+    # draw < fill = graphic processing speed
     pygame.display.update()
         
 pygame.quit()
